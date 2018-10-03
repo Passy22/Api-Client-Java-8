b53f23aa9b3d5ebb574568cba56960c41218d87c
Passy22 <sorisoriano@hotmail.com>
2018-10-03 11:16:34 +0200

    First Commit

---
+++ src/main/java/com/tecnopary/client/api/ClientStarter/App.java
@@ -0,0 +1,13 @@
+package com.tecnopary.client.api.ClientStarter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
